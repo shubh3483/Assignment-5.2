053b6eb8ce92e8bcac712d9fe38a3bbadbfadd58
Shubh Agrawal <shubh.coding@gmail.com>
2021-04-28 13:46:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/assignment5_2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.assignment5_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.assignment5_2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding b;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+        setUpEventHandlers();
+    }
+
+    private void setUpEventHandlers() {
+        b.toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast();    
+            }
+        });
+        b.countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCount();
+            }
+        });
+    }
+
+    private void showCount() {
+        b.countTextView.setText(""+count);
+        count++;
+    }
+
+    private void showToast() {
+        Toast toast = Toast.makeText(this,"Hello Toast",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
