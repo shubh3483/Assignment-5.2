186dbf78f55153f3db6c35edc50384b549b2816d
Shubh Agrawal <shubh.coding@gmail.com>
2021-04-28 16:31:56 +0530

    Completed Codelab 1.2.3

--- app/src/main/java/com/example/assignment5_2/MainActivity.java
+++ app/src/main/java/com/example/assignment5_2/MainActivity.java
@@ -11,52 +11,12 @@ import com.example.assignment5_2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding b;
-    int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(b.getRoot());
-        setUpEventHandlers();
-    }
-
-    private void setUpEventHandlers() {
-        b.toastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast();    
-            }
-        });
-        b.countButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showCount();
-            }
-        });
-        b.zeroButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showZero();
-            }
-        });
-    }
-
-    private void showZero() {
-        b.countTextView.setText(""+ 0);
-        count = 0;
-    }
-
-    private void showCount() {
-        b.countTextView.setText(""+count);
-        count++;
-    }
-
-    private void showToast() {
-        Toast toast = Toast.makeText(this,"Hello Toast",Toast.LENGTH_SHORT);
-        toast.show();
     }
 }
